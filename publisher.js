6759955d6c13f0979f3111f4139bb3dc0bf31c64
OskaMathis <oskamathis@gmail.com>
2020-06-21 22:27:09 +0900

    Publisherを実装

---
+++ publisher.js
@@ -0,0 +1,16 @@
+const mqtt = require('mqtt');
+const client = mqtt.connect({
+    host: 'localhost',
+    port: 1883,
+    clientId: 'mqtt.publisher',
+});
+
+client.on('connect', function(){
+    console.log('publisher.connected.');
+});
+
+setInterval(function(){
+    const message = Date.now().toString();
+    client.publish('topic0', message);
+    console.log('publisher.publish:', message);
+}, 1000);
